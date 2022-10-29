2c9c800654420a9d43aa52d6dc731d88ea56f8c5
yabr87 <108272773+yabr87@users.noreply.github.com>
2022-10-29 23:17:43 +0300

    start

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # goit-js-hw-06
+
+```js
+document.querySelector; // якщо нічого не знаходить повертає  null
+document.querySelectorAll; // якщо нічого не знаходить повертає []
+```
---
+++ notes.js
@@ -0,0 +1,16 @@
+const el = document.querySelector('.hero__title');
+el.document.querySelector; // якщо нічого не знаходить повертає  null
+document.querySelectorAll; // якщо нічого не знаходить повертає []
+
+el.textContent = 'text'; // змінює текст в середині єлементу
+el.getAtribute('src');
+
+/* Тоже что и */ el.src;
+
+el.setAtribute('src'); //Встановлює значення атрибута для зазначеного елемента. Якщо атрибут уже існує, значення оновлюється; інакше додається новий атрибут із зазначеним іменем і значенням.
+
+el.removeAttribute('src'); // видаляє атрибут
+el.hasAttribute('src'); // повертає true или false
+
+// https://www.youtube.com/watch?v=Haq2JIuE6Rg
+// 30:50  data атрибуты
