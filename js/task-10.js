b93ae67153f91aa163b77b55867e5e54784b04e9
yabr87 <108272773+yabr87@users.noreply.github.com>
2022-11-06 23:25:09 +0200

    fix task-10

--- js/task-10.js
+++ js/task-10.js
@@ -4,10 +4,11 @@ let numberOfboxes = 0;
 
 const form = document.querySelector('#controls');
 const divForBoxes = document.querySelector('#boxes');
-const ceateBtn = document.querySelector('[data-create]');
+const createBtn = document.querySelector('[data-create]');
 const desroyBtn = document.querySelector('[data-destroy]');
 
-ceateBtn.addEventListener('click', () => {
+createBtn.addEventListener('click', () => {
+  createBoxes();
   divForBoxes.insertAdjacentHTML('beforeend', arrBoxes.join(''));
   arrBoxes = [];
 });
@@ -19,6 +20,9 @@ desroyBtn.addEventListener('click', () => {
 
 form.firstElementChild.addEventListener('blur', (event) => {
   numberOfboxes = Number(event.currentTarget.value);
+});
+
+function createBoxes() {
   let counter = 0;
   while (counter < numberOfboxes) {
     arrBoxes.push(
@@ -28,7 +32,7 @@ form.firstElementChild.addEventListener('blur', (event) => {
     counter += 1;
     boxSize += 10;
   }
-});
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
